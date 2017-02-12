ad834736f1a26ac47765ef2d2125811098d4e820
GO <go05sato@gmail.com>
2017-02-13 02:01:26 +0900

    obj読み込み、OrbitControl安定版

--- threejs/Main.js
+++ threejs/Main.js
@@ -6,38 +6,88 @@ var container;
 var controls;
 var light;
 var sphere;
+var cameraRig;
+var effect;
+var stats;
 
-window.addEventListener("DOMContentLoaded", init, false);
+setup();
 
-
-function init()
-{
+// モデルのロードを待つ
+function setup()
+{   
+    // Scene
     scene = new THREE.Scene();
+    scene.visible = false;
 
+    // モデルの読み込み
+    var loader = new THREE.OBJMTLLoader();
+    loader.load( 'threejs/models/timelessness.obj', 'threejs/models/timelessness.mtl', onLoad);
+
+    function onLoad(object)
+    {
+        start(object);
+    }
+}
+
+// threejsの初期化処理
+function start(object)
+{
+    // camera
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 3000);
     camera.position.set(0, 0, 300);
     scene.add(camera);
 
-    var cameraRig = new THREE.Object3D();
+    // caemraRig
+    cameraRig = new THREE.Object3D();
     cameraRig.add(camera);
     scene.add(cameraRig);
 
+    // light
     light = new THREE.DirectionalLight(0xffffff, 1);
     light.position.set(0, 100, 0);
     scene.add(light);
 
+    // renderer
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setClearColor(0x000000);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+    // container
     element = renderer.domElement;
-
     container = document.getElementById("container");
     container.appendChild(element);
 
+    // effect
     effect = new THREE.StereoEffect(renderer);
     effect.separation = 0.06;
 
+    stats = new Stats();
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.top = "0px";
+    stats.domElement.style.zIndex = 100;
+    container.appendChild(stats.domElement);
+
+    createScene(object);
+}
+
+function createScene(object)
+{
+    var scale = 150;
+    object.scale.x = scale;
+    object.scale.y = scale;
+    object.scale.z = scale;
+    object.position.set(0, 100, 0);
+    scene.add(object);
+    onLoad();
+}
+
+function onLoad()
+{
+    window.addEventListener("resize", resize, false);
+    resize();
+    controls = new THREE.DeviceOrientationControls(cameraRig, true);
+    controls.connect();
+    
     /*マウス*/
 
     // controls = new THREE.OrbitControls(camera, element);
@@ -51,30 +101,17 @@ function init()
     // controls.noZoom = true;
     // controls.noPan = true;
 
-    /*ジャイロ*/
-
-    controls = new THREE.DeviceOrientationControls(cameraRig, true);
-    controls.connect();
-
-    window.addEventListener("resize", resize, false);
-    resize();
-
-    // window.addEventListener("deviceorientation", setOrientationControls, true);
-
-    loop();
-
-    /*関数*/
-    // createSphere();
-    objLoad();
-    createGrid();
-    createAxis();
+    createAxis()
+    animate();
+    scene.visible = true;
 }
 
-function loop()
+function animate()
 {
-    requestAnimationFrame(loop);
+    requestAnimationFrame(animate);
     controls.update();
     render();
+    stats.update();
 }
 
 function render()
@@ -82,18 +119,18 @@ function render()
     effect.render(scene, camera);
 }
 
-function setOrientationControls(e)
-{
-    if(!e.alpha)
-    {
-        return;
-    }
+// function setOrientationControls(e)
+// {
+//     if(!e.alpha)
+//     {
+//         return;
+//     }
 
-    controls = new THREE.DeviceOrientationControls(cameraRig, true);
-    controls.connect();
+//     controls = new THREE.DeviceOrientationControls(cameraRig, true);
+//     controls.connect();
 
-    window.removeEventListener("deviceorientation", setOrientationControls, true);
-}
+//     window.removeEventListener("deviceorientation", setOrientationControls, true);
+// }
 
 function resize()
 {
@@ -107,15 +144,7 @@ function resize()
     effect.setSize(width, height);
 }
 
-function objLoad()
-{   
-    var loader = new THREE.OBJMTLLoader();
-    loader.load( '../models/Robot_BI.obj', '../models/Robot_BI.mtl', function (object) {
-        object.position.set(0, 20, 0);
-        object.scale.set(20,20,20);
-        scene.add(object);
-    });
-}
+
 
 function createSphere()
 {
